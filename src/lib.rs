cb2b04d0018331d87dc6fcd0790edae39f8560d5
tmvkrpxl0 <tmvkrpxl0@proton.me>
2024-11-18 15:41:08 +0900

    [WIP]Adding trait and macros for enum_variant_map create...

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-//! A simple derive macro to generate an `ordinal()`
-//! method for enums.
+//! A simple derive macro to generate `ordinal()` and `count()`
+//! methods for enums. This is used as index and size for
+//! enum_variant_map internally.
 //!
 //! Unlike `num_derive::ToPrimitive`, this derive macro
 //! allows non-C-like enums. The `ordinal` function reflects
@@ -8,8 +9,10 @@
 //!
 //! # Example
 //! ```
-//! use ordinalizer::Ordinal;
-//! #[derive(Ordinal)]
+//! use enum_variant_map_macro::VariantMap;
+//! use enum_variant_map_types::VariantMap;
+//!
+//! #[derive(VariantMap)]
 //! enum Animal {
 //!     Dog,
 //!     Cat {
@@ -17,14 +20,30 @@
 //!     }
 //! }
 //!
+//! // `ordinal()` ignores assigned values and representation's type
+//! #[repr(i32)]
+//! #[derive(VariantMap)]
+//! enum PrimitiveEnum {
+//!     A = 1,
+//!     B = 21,
+//!     C = 31
+//! }
+//!
+//! assert_eq!(Animal::COUNT, 2);
 //! assert_eq!(Animal::Dog.ordinal(), 0);
 //! assert_eq!((Animal::Cat { age: 10 }).ordinal(), 1);
+//!
+//! assert_eq!(PrimitiveEnum::COUNT, 3);
+//! assert_eq!(PrimitiveEnum::A.ordinal(), 0usize);
+//! assert_eq!(PrimitiveEnum::B.ordinal(), 1usize);
+//! assert_eq!(PrimitiveEnum::C.ordinal(), 2usize);
 //! ```
 
 use proc_macro2::{Ident, TokenStream};
 use proc_macro_error::*;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
+use syn::parse::{Parse, ParseStream};
 
 struct Variant<'a> {
     ident: &'a Ident,
@@ -32,7 +51,7 @@ struct Variant<'a> {
     has_named_fields: bool,
 }
 
-/// Generates a `fn ordinal(&self) -> usize` for an enum.
+/// Generates `fn ordinal(&self) -> usize` and `COUNT` constants for an enum.
 ///
 /// The enum may have any number of variants. It is not
 /// required to be a C-like enum, i.e. its variants
@@ -42,8 +61,8 @@ struct Variant<'a> {
 /// index in the enum definition. For example, the first
 /// variant of enum will have ordinal `0`.
 #[proc_macro_error]
-#[proc_macro_derive(Ordinal)]
-pub fn derive_ordinal(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(VariantMap)]
+pub fn derive_variant_map(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let variants = detect_variants(&input);
@@ -51,16 +70,21 @@ pub fn derive_ordinal(input: proc_macro::TokenStream) -> proc_macro::TokenStream
     let match_arms = generate_match_arms(&variants, &input);
 
     let enum_ident = &input.ident;
+    let generics = &input.generics;
 
+    let count = variants.len();
     let tokens = quote! {
-        impl #enum_ident {
-            pub fn ordinal(&self) -> usize {
+        impl#generics enum_variant_map_types::VariantMap for #enum_ident #generics {
+            const COUNT: usize = #count;
+
+            fn ordinal(&self) -> usize {
                 match self {
                     #(#match_arms,)*
                 }
             }
         }
     };
+
     tokens.into()
 }
 
@@ -102,16 +126,14 @@ fn generate_match_arms(variants: &[Variant], input: &DeriveInput) -> Vec<TokenSt
     for (ordinal, variant) in variants.iter().enumerate() {
         let variant_ident = variant.ident;
         let pattern = match (variant.has_named_fields, variant.unit_field_count) {
-            (true, _) => quote! { #enum_ident::#variant_ident { .. } },
-            (false, x) if x != 0 => {
+            (true, _) | (false, 0) => quote! { #enum_ident::#variant_ident { .. } },
+            (false, x) => {
                 let underscores: Vec<_> = (0..x).map(|_| quote! { _ }).collect();
 
                 quote! {
                     #enum_ident::#variant_ident(#(#underscores),*)
                 }
             }
-            (false, 0) => quote! { #enum_ident::#variant_ident },
-            _ => unreachable!(),
         };
 
         vec.push(quote! {
@@ -120,4 +142,20 @@ fn generate_match_arms(variants: &[Variant], input: &DeriveInput) -> Vec<TokenSt
     }
 
     vec
+}
+
+#[proc_macro]
+pub fn get(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as VariantMapGetter);
+}
+
+struct VariantMapGetter {
+    map: Ident,
+    colon: syn::Path
+}
+
+impl Parse for VariantMapGetter {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        todo!()
+    }
 }
